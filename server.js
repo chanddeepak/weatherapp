857db8247dfbdf1d5036eff643277e1368145812
Deepak Chand <deepak.chand@nagarro.com>
2019-04-25 18:01:12 +0530

    commit
    Signed-off-by: Deepak Chand <deepak.chand@nagarro.com>

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/weathersapp'));
+app.use(express.static(__dirname + '/dist/weatherapp'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/weathersapp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/weatherapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
